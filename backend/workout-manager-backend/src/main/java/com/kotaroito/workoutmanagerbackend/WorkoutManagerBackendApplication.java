3535a7ee94df98d93554be9aab6b77ea5a707ab0
kotaroito <agkito34@gmail.com>
2023-11-23 23:27:50 -0800

    intialize backend

---
+++ backend/workout-manager-backend/src/main/java/com/kotaroito/workoutmanagerbackend/WorkoutManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kotaroito.workoutmanagerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutManagerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutManagerBackendApplication.class, args);
+	}
+
+}
---
+++ backend/workout-manager-backend/src/test/java/com/kotaroito/workoutmanagerbackend/WorkoutManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kotaroito.workoutmanagerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutManagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
